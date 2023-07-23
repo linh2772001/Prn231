b53e6cfabc52cbce37ea56dbb594a0833956652a
ngangiang2909 <ngangiang2909@gmail.com>
2023-07-23 15:01:41 +0700

    Update product in manager nhung dang bi loi o phan foreach

---
+++ PetStoreClient/Controllers/ProductController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PetStoreClient.Controllers
+{
+    public class ProductController : Controller
+    {
+       // [Authorize(Roles ="1")]
+        public async Task<IActionResult> Index(int id)
+        {
+            return View();
+        }
+    }
+}
