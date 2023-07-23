22465cc94f7615a4ca4a9309b6b1c9a7cc643279
ngangiang2909 <ngangiang2909@gmail.com>
2023-07-23 19:29:07 +0700

    Update api

---
+++ BusinessObject/Models/Cart.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessObject.Models
+{
+    public class Cart
+    {
+        public Product Product { get; set; }
+        public int Quantity { get; set; }
+        public Cart()
+        {
+
+        }
+        public Cart(Product product, int quantity)
+        {
+            Product = product;
+            Quantity = quantity;
+        }
+    }
+}
--- BusinessObject/Models/ProjectContext.cs
+++ BusinessObject/Models/ProjectContext.cs
@@ -37,7 +37,7 @@ namespace BusinessObject.Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("server=localhost\\SQLEXPRESS;database=Project;Integrated security=true");
+                optionsBuilder.UseSqlServer("server=localhost;database=Project;Integrated security=true");
             }
         }
 
